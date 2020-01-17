2eebf47bc889de38c77a6d81696a6a862b5d3eab
adelaney11 <adelaney3@luc.edu>
2020-01-17 09:24:59 -0600

    Add Finished Apps

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Adam Delaney on 6/7/19.
+//  Copyright © 2019 Adam Delaney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
